fbb168c34c281815eac69eb9321cf37fe15b688c
Alexis Graff <alexisgraff123@gmail.com>
2022-12-21 21:06:12 -0300

    Cominezo de Inicio

--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -1,5 +1,19 @@
+import './inicio.css'
+
 export const Inicio = () => {
     return (
-        <div>Soy inicio</div>
+        <div className="container-inicio">
+            <div className="inicio-a">
+                <div className="inicio-a-title"><h1>Somos Panambí</h1></div>
+                <div className="inicio-a-parrafo">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+                        architecto asperiores blanditiis
+                        commodi cum delectus dolor, eveniet excepturi iure labore, mollitia nihil nisi perspiciatis
+                        placeat
+                        quidem repellat, sed vel veniam?
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
